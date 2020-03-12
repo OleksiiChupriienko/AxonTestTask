78856f74223225d969a02b4d87382359b37e90be
Aleksei <lesha.chupritnko@gmail.com>
2020-03-12 06:02:05 +0200

    project is under source control

---
+++ AxonTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AxonTestTask
+//
+//  Created by Aleksei Chupriienko on 12.03.2020.
+//  Copyright © 2020 Aleksei Chupriienko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
